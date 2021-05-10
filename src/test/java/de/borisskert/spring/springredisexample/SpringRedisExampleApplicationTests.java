9817053add16808e1b80d490e35c1680527f34fd
Boris Skert <boris.skert@gmail.com>
2021-05-10 09:19:01 +0200

    Create project with Spring Initializr

---
+++ src/test/java/de/borisskert/spring/springredisexample/SpringRedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.spring.springredisexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
